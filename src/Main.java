e2d11292fbe196876bb7080c44bbca9f4926f25c
chayanonc <chayanonc.ph@gmail.com>
2025-05-28 17:19:54 +0700

    Threading

--- src/Main.java
+++ src/Main.java
@@ -51,7 +51,26 @@ public class Main {
     }
 
     public static void main(String[] args) {
-//Chapter 41 [Enums]
+//
+// Chapter 42 [Threading]
+        Scanner scanner = new Scanner(System.in);
+
+        MyRunnable myRunnable = new MyRunnable();
+        Thread thread = new Thread(myRunnable);
+
+        thread.setDaemon(true);
+        thread.start();
+
+        System.out.println("You have 5 sec to enter your name");
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+
+        scanner.close();
+
+
+
+/*Chapter 41 [Enums]
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a day of the week: ");
         String input = scanner.nextLine().toUpperCase();
@@ -70,7 +89,6 @@ public class Main {
             System.out.println("Error Input");
         }
 
-/*
 Chapter 40 [Hashmap]
         HashMap<String, Double> map = new HashMap<>();
         map.put("apple", 100.99);
